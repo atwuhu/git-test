c5b43bbbfb5f32a08a656f68702013facf4927ff
wuhu <wuhu@123.com>
2022-07-23 22:41:21 +0800

    master commit2

--- src/main/java/com/wuhu/GitTest.java
+++ src/main/java/com/wuhu/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello wuhugit");
         System.out.println("hello wuhugit2");
         System.out.println("hello wuhugit3");
+        System.out.println("hello wuhugit4");
+        System.out.println("master");
     }
 }
