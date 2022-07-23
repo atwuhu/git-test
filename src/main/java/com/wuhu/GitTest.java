d7171f9cc552dff285dac8324742c126a790fc9e
wuhu <wuhu@123.com>
2022-07-23 22:35:47 +0800

    hot-fix commit1

--- src/main/java/com/wuhu/GitTest.java
+++ src/main/java/com/wuhu/GitTest.java
@@ -3,7 +3,8 @@ package com.wuhu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello wuhugit");
-        System.out.println("hello wuhugit");
+        System.out.println("hello wuhugit2");
+        System.out.println("hello wuhugit3");
         System.out.println("hello wuhugit4");
     }
 }
