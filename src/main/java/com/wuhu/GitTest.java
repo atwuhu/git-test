fda4369946373ab6509d9f732e1057fcb1937c7b
wuhu <wuhu@123.com>
2022-07-23 22:40:24 +0800

    hot-fix commit2

--- src/main/java/com/wuhu/GitTest.java
+++ src/main/java/com/wuhu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello wuhugit2");
         System.out.println("hello wuhugit3");
         System.out.println("hello wuhugit4");
+        System.out.println("hot");
     }
 }
