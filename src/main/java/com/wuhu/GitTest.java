2871dee7ade4dc478ff87d5230582ec41fa945f8
wuhu <wuhu@123.com>
2022-07-23 19:14:43 +0800

    second commit

--- src/main/java/com/wuhu/GitTest.java
+++ src/main/java/com/wuhu/GitTest.java
@@ -3,5 +3,7 @@ package com.wuhu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello wuhugit");
+        System.out.println("hello wuhugit");
+        System.out.println("hello wuhugit");
     }
 }
