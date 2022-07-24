fe20e97ef4222bfd96b992918fe3088e4390c9f7
wuhu <wuhu@123.com>
2022-07-24 09:28:05 +0800

    push test

--- src/main/java/com/wuhu/GitTest.java
+++ src/main/java/com/wuhu/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello wuhugit4");
         System.out.println("master");
         System.out.println("push test412132324");
+        System.out.println("test1 test412132324");
+
     }
 }
